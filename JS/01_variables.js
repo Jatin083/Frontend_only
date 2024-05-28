299bf4ee9e9c358d62e32a07587efbf3fff2d26f
jatin <125881431+Jatin083@users.noreply.github.com>
2024-05-28 09:37:53 +0530

    Add files via upload

---
+++ JS/01_variables.js
@@ -0,0 +1,23 @@
+const accountID = 144553;   // const values are not changeable
+let accountEmail = "jatinsingh32063@gmail.com"; 
+var accountPassword  = "JJTTOO@32";  // var is not used now because of the scope issue
+let Name;
+
+
+accountCity = "Delhi" // you can also declare things like this but it's not a correct way 
+
+// accountID = 2 //Not allowed
+
+accountEmail = "YOdbfhch@gmail.com";
+accountPassword = "4563546";
+accountCity = "Jaipur"
+
+console.log(accountID); // print the output 
+
+console.table([accountID,accountEmail,accountPassword, accountCity, Name]); // console.table makes tables with output
+
+
+/*
+1. only use let and const 
+2. prefer not use var because of issue in block and functional scope
+*/
