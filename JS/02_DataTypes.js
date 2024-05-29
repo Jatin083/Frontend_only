38d982edf2e64aab2a452e7fde6da69e0aa8794b
jatin <125881431+Jatin083@users.noreply.github.com>
2024-05-29 19:27:24 +0530

    Add files via upload

---
+++ JS/02_DataTypes.js
@@ -0,0 +1,25 @@
+// Datatypes
+
+"use strict"; // treat all js code as newer version
+
+// alert("hello") // we are using nodejs not browser
+
+console.log(3+3);
+console.log("Jatin");
+
+let Name = "Jatin";
+let age = 19;
+let isLoggedIn = false;
+
+
+// number => 2 to power 53
+// bigint =>
+// string => ""
+// boolean => yes/No
+// Null => standalone                          // ek object h 
+// undefined => value is not defined             // iska type hi undefined hai
+// symbol => unique
+
+// object =>                                    // iska type function hai
+
+console.log(typeof(Object));
