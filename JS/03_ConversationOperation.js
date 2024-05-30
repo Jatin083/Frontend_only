84eeea1a24993e709a562fa99306657984334bb0
jatin <125881431+Jatin083@users.noreply.github.com>
2024-05-30 10:30:52 +0530

    Add files via upload

---
+++ JS/03_ConversationOperation.js
@@ -0,0 +1,76 @@
+let score = "33abc"
+
+// console.log(typeof score);
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+
+// console.log(valueInNumber); //return NaN not a number 
+
+
+// "33" => 33
+// "33abc" => NaN
+// true => 1; false => 0
+
+let isloggedin = "j";
+
+let Booleanisloggedin = Boolean(isloggedin)
+// console.log(Booleanisloggedin)
+// console.log(typeof(Booleanisloggedin))
+
+// 1 => true; 0 => false
+// "" => false
+// "Jatin" =  True
+
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof(stringNumber));
+
+
+
+// ************************************* Operations ***********************************************************\\
+
+let value = 3
+let negvalue = -value
+// console.log(negvalue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/3);
+// console.log(2%3);
+
+
+let a = "hi "
+let b = "there"
+let c = a + b
+// console.log(c);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2 );
+// console.log(1 + 2 + "2");
+
+// console.log(+true);
+// console.log(+"");
+
+
+let num1, num2, num3
+num1 = num2 = num3 = 2 + 2
+// console.table([num1, num2, num3])
+
+
+let gamecounter = 100;
+gamecounter;
+console.log(gamecounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.
+
+// and learn prefix and postfix on mdn
