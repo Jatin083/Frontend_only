d10a058a3ba8826ea9b1601c8486a09aede5ab67
jatin <125881431+Jatin083@users.noreply.github.com>
2024-06-07 05:21:02 +0530

    Add files via upload

---
+++ GSAP/1st_GSAP_/script.js
@@ -0,0 +1,125 @@
+var tl = gsap.timeline();
+
+tl.from("h2",{
+    y:-30,
+    opacity:0,
+    duration:1,
+    delay:0.5
+})
+tl.from("h4",{
+    y:-20,
+    opacity:0,
+    duration:1,
+    stagger:0.3
+})
+tl.from("h1",{
+    y:20,
+    opacity:0,
+    duration:1,
+    scale:0.2
+})
+
+
+
+
+
+
+
+
+
+
+// // var tl = gsap.timeline();
+
+// // tl.to("#box1",{
+// //     x:1200,
+// //     rotate:360,
+// //     duration:1.5,
+// //     delay:1
+// // })
+
+// // tl.to("#box2",{
+// //     x:1200,
+// //     duration:1.5,
+// // })
+
+// // tl.to("#box3",{
+// //     x:1500,
+// //     backgroundColor:"pink",
+// //     duration:2,
+// // })
+
+// // gsap.to("#box1",{
+// //     x:1200,
+// //     rotate:360,
+// //     duration:1.5,
+// //     delay:1,
+// // })
+// // gsap.to("#box2",{
+// //     x:1500,
+// //     backgroundColor:"yellow",
+// //     duration:1.5,
+// //     delay:2.5,
+// // })
+
+
+
+
+
+// // gsap.to("#box",{
+// //     x:1200,
+// //     duration:1,
+// //     delay:1,
+// //     rotate:360,
+// //     repeat:-1,
+// //     yoyo:true
+// // })
+
+
+
+
+
+
+
+// gsap.from("h1",{
+//     opacity:0,
+//     y:20,
+//     duration:2,
+//     dilay:1,
+//     stagger:0.3
+// })
+
+
+
+
+
+// // gsap.to("#box1",{
+// //     x:1200, //movement in x-axis
+// //     duration:2, // how much time take 
+// //     delay:1 // how mutch delay in animation
+// // })
+// // gsap.from("#box2",{
+// //     y:1200, //movement in y-axis
+// //     duration:2, // how much time take 
+// //     delay:1 // how mutch delay in animation
+// // })
+
+
+// // gsap.to("#box1",{
+// //         x:1200, //movement in x-axis
+// //         duration:2, // how much time take 
+// //         delay:1,
+// //         rotate:360,
+// //         backgroundColor:"green",
+// //         borderRadius:"50%",
+// //         scale:2
+// //     })
+// //     gsap.from("#box2",{
+// //         x:1200, //movement in x-axis
+// //         duration:2, // how much time take 
+// //         delay:1,
+// //         rotate:360,
+// //         backgroundColor:"green",
+// //         borderRadius:"50%",
+// //         scale:2
+// //     })
+
