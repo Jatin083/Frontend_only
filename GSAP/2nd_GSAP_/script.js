3c735a616b3329029b0a2a1aca253e3fd5fadcd0
jatin <125881431+Jatin083@users.noreply.github.com>
2024-06-08 10:06:35 +0530

    Add files via upload

---
+++ GSAP/2nd_GSAP_/script.js
@@ -0,0 +1,83 @@
+gsap.from("#page2 #box",{
+    scale:0,
+    opacity:0,
+    duration:1,
+    rotate:720,
+    scrollTrigger:{
+        trigger:"#page2 box",
+        scroller:"body",
+        markerd
+
+    }
+})
+
+
+
+
+
+
+
+
+
+// gsap.from("#page2 h1",{
+//     opacity:0,
+//     duration:2,
+//     x:500,
+//     scrollTrigger:{
+//         trigger:"#page2 h1",
+//         scroll:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+
+// })
+
+// gsap.from("#page2 h2",{
+//     opacity:0,
+//     duration:2,
+//     x:-500,
+//     scrollTrigger:{
+//         trigger:"#page2 h2",
+//         scroll:"body",
+//         markers:true,
+//         start:"top 50%"
+//     }
+
+// })
+
+
+
+
+
+
+
+
+
+
+
+// gsap.from("#page1 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360
+// })
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:"#page2 box",
+// })
+
+// gsap.from("#page2 #box",{
+//     scale:0,
+//     delay:1,
+//     duration:2,
+//     rotate:360,
+//     scrollTrigger:{
+//         trigger:"#page2 #box",
+//         scroller:"body",
+//         markers:true,
+//         start:"top 60%"
+//     }
+// })
