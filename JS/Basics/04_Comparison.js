7d9cc8064dcf647f766c52271cf084e12cd94b7d
jatin <125881431+Jatin083@users.noreply.github.com>
2024-06-30 09:01:45 +0530

    Update 04_Comparison.js

--- JS/Basics/04_Comparison.js
+++ JS/Basics/04_Comparison.js
@@ -14,3 +14,8 @@ console.log(null >= 0);  //true  null is converted into 0
 console.log(undefined == 0);//false
 console.log(undefined > 0);//false
 console.log(undefined < 0);//false
+
+// === strictly check karega or datatype bhi dekhega
+//  == don't see datatype and converstion is possible
+
+console.log("2" === 2);
