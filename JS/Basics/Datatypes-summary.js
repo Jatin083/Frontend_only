79951b1c435d264127052bbf109c33fcac94bdc2
jatin <125881431+Jatin083@users.noreply.github.com>
2024-06-30 09:04:28 +0530

    Update Datatypes-summary.js

--- JS/Basics/Datatypes-summary.js
+++ JS/Basics/Datatypes-summary.js
@@ -13,4 +13,20 @@ const id = Symbol('123')
 const anotherid = Symbol('123')
 console.log(id === anotherid);
 
+const bigNumber = 45746572783728748n
+// Refrence (Non primitive)
+
+// Array, objects, Functions
+
+const heros = ["Thor", "IronMan", "batman"]
+
+let myObj = {
+    name:"Jatin",
+    age:19,
+}
+const myFunction = function(){
+    console.log("Hello bhai kya haal hai");
+}
+myFunction();
+
 
