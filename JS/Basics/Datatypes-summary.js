0cec2118a8f8d260d8908b9a2c21e2d0b49b469d
jatin <125881431+Jatin083@users.noreply.github.com>
2024-06-30 09:04:13 +0530

    Update Datatypes-summary.js

--- JS/Basics/Datatypes-summary.js
+++ JS/Basics/Datatypes-summary.js
@@ -5,3 +5,12 @@
 const score =100;
 const scoreValue = 100.3;
 
+const isLoggedIn = false;
+const outsidetemp = null;
+let useremail;
+
+const id = Symbol('123')
+const anotherid = Symbol('123')
+console.log(id === anotherid);
+
+
