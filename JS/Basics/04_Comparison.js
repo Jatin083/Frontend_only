12f7de423e1d54d249439b6f94dfcbd42434489c
jatin <125881431+Jatin083@users.noreply.github.com>
2024-06-30 09:01:28 +0530

    Update 04_Comparison.js

--- JS/Basics/04_Comparison.js
+++ JS/Basics/04_Comparison.js
@@ -7,3 +7,10 @@
 console.log("2" + 1);
 console.log(2 + "1");
 
+console.log(null > 0); //false                                      -
+console.log(null == 0);//false
+console.log(null >= 0);  //true  null is converted into 0 
+
+console.log(undefined == 0);//false
+console.log(undefined > 0);//false
+console.log(undefined < 0);//false
