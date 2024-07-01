893d2dff4bd2ae673a9e83e6e08c5c59b0b6e01f
jatin <125881431+Jatin083@users.noreply.github.com>
2024-07-01 06:09:52 +0530

    Update 05_Strings.js

--- JS/Basics/05_Strings.js
+++ JS/Basics/05_Strings.js
@@ -7,3 +7,11 @@ console.log(`Hello mu name is ${name} and my repo count is ${repoCount}`);
 
 const gameName = new String('Jatinsingh')
 
+console.log(gameName.__proto__);//{}
+console.log(gameName.length);//10
+console.log(gameName.toUpperCase());//JATINSINGH
+console.log(gameName.charAt(2));//t
+console.log(gameName.indexOf('t'));//2
+
+const newString = gameName.substring(0,4)
+console.log(newString);//Jati
