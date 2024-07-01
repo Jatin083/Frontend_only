6eec01acdeec1bf6f5c8074bc6b0f6329b0dc6e8
jatin <125881431+Jatin083@users.noreply.github.com>
2024-07-01 06:09:34 +0530

    Update 05_Strings.js

--- JS/Basics/05_Strings.js
+++ JS/Basics/05_Strings.js
@@ -3,3 +3,7 @@ const repoCount = 50
 
 // console.log(name + repoCount + "Value");   // wrong way
 
+console.log(`Hello mu name is ${name} and my repo count is ${repoCount}`);
+
+const gameName = new String('Jatinsingh')
+
